71f24aa68543d61175430b175b646d9558df0445
niba <kamil.nibisz@gmail.com>
2016-07-12 20:14:48 +0200

    rename project

--- BitbucketVS.VisualStudio.UI/Properties/AssemblyInfo.cs
+++ BitbucketVS.VisualStudio.UI/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BitBucketVs.UI")]
+[assembly: AssemblyTitle("VSIXProject1")]
 [assembly: AssemblyDescription("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -23,3 +23,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
