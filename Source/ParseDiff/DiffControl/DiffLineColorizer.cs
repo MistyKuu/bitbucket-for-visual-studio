70c90966e1e4c6e35bfc0507622e17f1a3b97dc8
MistyKu <mistyksu@gmail.com>
2016-08-14 20:44:09 +0200

    fix theming in avalonedit

---
+++ Source/ParseDiff/DiffControl/DiffLineColorizer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using ICSharpCode.AvalonEdit.Rendering;
+
+namespace ParseDiff.DiffControl
+{
+    public class DiffLineColorizer : DocumentColorizingTransformer
+    {
+        private readonly ChunkDiff _chunkDiff;
+
+        public DiffLineColorizer(ChunkDiff chunkDiff)
+        {
+            _chunkDiff = chunkDiff;
+        }
+
+        protected override void ColorizeLine(ICSharpCode.AvalonEdit.Document.DocumentLine line)
+        {
+            var change = _chunkDiff.Changes[line.LineNumber - 1];
+            if (!line.IsDeleted && (change.Type == LineChangeType.Add || change.Type == LineChangeType.Delete))
+            {
+                ChangeLinePart(line.Offset, line.EndOffset, el =>
+                {
+                    el.TextRunProperties.SetForegroundBrush(Brushes.Black);
+                });
+            }
+        }
+    }
+}
