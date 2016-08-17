56494d3f4e08575fef0780f14de478fe742aeb27
niba <kamil.nibisz@gmail.com>
2016-08-17 18:44:00 +0200

    Fix tests

--- Source/Tests/Integration/Bitbucket.REST.API.Integration.Tests/Clients/RepositoryClientTests.cs
+++ Source/Tests/Integration/Bitbucket.REST.API.Integration.Tests/Clients/RepositoryClientTests.cs
@@ -20,7 +20,7 @@ namespace Bitbucket.REST.API.Integration.Tests.Clients
             var credentials = new Credentials(CredentialsHelper.TestsCredentials.Username, CredentialsHelper.TestsCredentials.Password);
             var connection = new Connection(credentials);
 
-            bitbucketClient = new BitBucket.REST.API.BitbucketClient(connection);
+            bitbucketClient = new BitBucket.REST.API.BitbucketClient(connection, connection);
         }
 
         [Test]
