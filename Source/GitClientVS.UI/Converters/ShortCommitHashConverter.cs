804f0ab7c8736c9707e64eec1beb7b5cbf6a69e9
niba <kamil.nibisz@gmail.com>
2016-08-17 13:59:25 +0200

    Redesign commit view according to bitbucket style

---
+++ Source/GitClientVS.UI/Converters/ShortCommitHashConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace GitClientVS.UI.Converters
+{
+    public class ShortCommitHashConverter : BaseMarkupExtensionConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return "";
+
+            var commitHash = (string) value;
+
+
+            return commitHash.Substring(0, 6);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
