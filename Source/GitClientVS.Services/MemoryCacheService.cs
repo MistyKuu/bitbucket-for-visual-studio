1b7ba03b2ac5d558ace5fdaff79db6aefb4a8cb4
MistyKu <mistyksu@gmail.com>
2016-08-17 18:39:31 +0200

    merge

---
+++ Source/GitClientVS.Services/MemoryCacheService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitClientVS.Contracts.Interfaces.Services;
+using ParseDiff;
+
+namespace GitClientVS.Services
+{
+    [Export(typeof(IDiffFileParser))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class MemoryCacheService : IMemoryCacheService
+    {
+        //public IEnumerable<FileDiff> Parse(string diff)
+        //{
+            
+        //}
+    }
+
+    public interface IMemoryCacheService
+    {
+    }
+}
