73ae23ac9fbd305564e83e54a5711177ccac8c1c
MistyKu <mistyksu@gmail.com>
2016-08-20 20:25:00 +0200

    asdasda

--- Source/GitClientVS.UI/Helpers/VisualTreeHelpers.cs
+++ Source/GitClientVS.UI/Helpers/VisualTreeHelpers.cs
@@ -25,5 +25,25 @@ namespace GitClientVS.UI.Helpers
             else
                 return FindParent<T>(parentObject);
         }
+
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
     }
 }
