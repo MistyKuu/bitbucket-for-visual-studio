2d6d002a9383c3b6d549599b2de1000cd5cf03d2
MistyK <mistyksu@gmail.com>
2017-03-27 20:39:04 +0200

    fix branch titles

---
+++ Source/GitClientVS.UI/Converters/StringToShortStringConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitClientVS.UI.Converters
+{
+    public class StringToShortStringConverter : BaseMarkupExtensionConverter
+    {
+        private readonly int _maxLength;
+
+        public StringToShortStringConverter(int maxLength)
+        {
+            _maxLength = maxLength;
+        }
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            var str = (string) value;
+            if (str.Length <= _maxLength)
+                return str;
+
+            return str.Substring(0, _maxLength);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
