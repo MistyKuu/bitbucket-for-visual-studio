5fff8988d76a5d574d399a7d9e29560c2ab9cabd
MistyK <mistyksu@gmail.com>
2017-05-21 19:39:39 +0200

    revert accidently removed extensions

---
+++ Source/GitClientVS.Infrastructure/Extensions/IServiceProviderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitClientVS.Infrastructure.Extensions
+{
+    public static class IServiceProviderExtensions
+    {
+        public static T GetService<T>(this IServiceProvider serviceProvider) where T : class
+        {
+            return serviceProvider.GetService(typeof(T)) as T;
+        }
+        public static TBase GetService<TChild, TBase>(this IServiceProvider serviceProvider) where TChild : class where TBase : class
+        {
+            return serviceProvider.GetService(typeof(TChild)) as TBase;
+        }
+    }
+}
